c2d4cd3e55ac265f9417362f076e72c14329e609
诸葛子房 <xiaoqiu2017wy@163.com>
2025-05-14 19:21:56 +0800

    feat:faq

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -29,40 +29,6 @@
 <template>
 
   <div class="page-footer">
-    <div>
-  <h3>What is a Virtual Piano?</h3>
-<p>A Virtual Piano is an online tool that allows you to play piano right from your computer or mobile device. You can use your keyboard to play notes, just like a real piano. It's a fun way to create music and practice without needing a physical piano!
-</p>
-
-<h3>Can I access the Virtual Piano from anywhere?</h3>
-<p>Yes, you can access the Virtual Piano from anywhere in the world. If you're not in mainland China, you can use our mirror website for faster access. Just open the link and start playing!
-</p>
-
-<h3>How do I play notes on the Virtual Piano?</h3>
-<p>You can play notes using the keyboard on your device! Each key on your keyboard corresponds to a note on the piano. For example, if you press the 'A' key, it will produce the note 'La'. Have fun exploring different notes and creating your own music!
-</p>
-
-<h3>Do I need any special equipment to use the Virtual Piano?</h3>
-No special equipment is needed! You can use your computer or mobile device with a standard keyboard. However, if you have a MIDI keyboard, you can connect it to your device for an even better experience.
-
-<h3>Is it free to use the Virtual Piano?<h3>
-Yes, playing the Virtual Piano is completely free! You can play anytime you want, without any charges. Just visit the website, and you're ready to go!
-
-</h3>What if my browser doesn't support the Virtual Piano?<h3>
-If your browser doesn't support the Virtual Piano, we recommend using Google Chrome or Opera, as they work best with our platform. Make sure to update to the latest version for the best experience!
-
-<h3>Can I create my own music sheets?<h3>
-Absolutely! You can create your own music sheets using our tool. Just visit the 'Create Music Sheet' section on our website and follow the steps to compose your own music.
-
-<h3>Is there a mobile app for the Virtual Piano?</h3>
-Yes, there is a mobile app for the Virtual Piano! You can scan the QR codes provided on the website to download the app on your mobile device and play piano on the go.
-
-<h3>What types of instruments can I use with the Virtual Piano?<h3>
-The Virtual Piano offers a variety of instrument sounds. You can choose from pianos, guitars, and even unique sounds like music boxes or xylophones! Try them out to see which sounds you like best.
-
-<h3>Can beginners use the Virtual Piano?</h3>
-Yes, beginners can easily use the Virtual Piano! The design is simple and user-friendly, making it perfect for anyone who wants to learn and have fun playing music.
-    </div>
     <a href="https://www.suncreativesolution.com/#/project/clavier" target="_blank" class="record-info">By Sun</a>
     <a href="https://github.com/WarpPrism/AutoPiano" class="record-info">&copy;Script credit: 晶体世界</a>
     <ul class="footer-ul">
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,6 +11,41 @@
     <section v-if="isReaingOnline">
        <AudioPlayer :para-text="textStore" :type="audioType"></AudioPlayer>
     </section>
+    <div id="faqId">
+    <h3>What is a Virtual Piano?</h3>
+  <p>A Virtual Piano is an online tool that allows you to play piano right from your computer or mobile device. You can use your keyboard to play notes, just like a real piano. It's a fun way to create music and practice without needing a physical piano!
+  </p>
+
+  <h3>Can I access the Virtual Piano from anywhere?</h3>
+  <p>Yes, you can access the Virtual Piano from anywhere in the world. If you're not in mainland China, you can use our mirror website for faster access. Just open the link and start playing!
+  </p>
+
+  <h3>How do I play notes on the Virtual Piano?</h3>
+  <p>You can play notes using the keyboard on your device! Each key on your keyboard corresponds to a note on the piano. For example, if you press the 'A' key, it will produce the note 'La'. Have fun exploring different notes and creating your own music!
+  </p>
+
+  <h3>Do I need any special equipment to use the Virtual Piano?</h3>
+  <p>No special equipment is needed! You can use your computer or mobile device with a standard keyboard. However, if you have a MIDI keyboard, you can connect it to your device for an even better experience.
+  </p>
+  <h3>Is it free to use the Virtual Piano?<h3>
+  <p>Yes, playing the Virtual Piano is completely free! You can play anytime you want, without any charges. Just visit the website, and you're ready to go!
+  </p>
+  </h3>What if my browser doesn't support the Virtual Piano?<h3>
+  <p>If your browser doesn't support the Virtual Piano, we recommend using Google Chrome or Opera, as they work best with our platform. Make sure to update to the latest version for the best experience!
+  </p>
+  <h3>Can I create my own music sheets?<h3>
+  <p>Absolutely! You can create your own music sheets using our tool. Just visit the 'Create Music Sheet' section on our website and follow the steps to compose your own music.
+  </p>
+  <h3>Is there a mobile app for the Virtual Piano?</h3>
+  <p>Yes, there is a mobile app for the Virtual Piano! You can scan the QR codes provided on the website to download the app on your mobile device and play piano on the go.
+  </p>
+  <h3>What types of instruments can I use with the Virtual Piano?<h3>
+  <p>The Virtual Piano offers a variety of instrument sounds. You can choose from pianos, guitars, and even unique sounds like music boxes or xylophones! Try them out to see which sounds you like best.
+</p>
+  <h3>Can beginners use the Virtual Piano?</h3>
+  <p>Yes, beginners can easily use the Virtual Piano! The design is simple and user-friendly, making it perfect for anyone who wants to learn and have fun playing music.
+  </p>
+  </div>
     <PageFooter></PageFooter>
   </div>
 </template>
